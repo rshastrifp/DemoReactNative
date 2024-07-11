6c03269a49f4d7df3019041211e8ad69589fa5a3
Ronak <ronak@ronaks-laptop.home>
2024-07-10 22:38:05 -0400

    add section list componet example

--- ios/components/MenuItems.js
+++ ios/components/MenuItems.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, SectionList } from 'react-native';
 
-const menuItemsToDisplay = [
+const menuItemsToDisplay_old = [
   { name: 'Hummus', price: '$5.00', id: '1A' },
   { name: 'Moutabal', price: '$5.00', id: '2B' },
   { name: 'Falafel', price: '$7.50', id: '3C' },
@@ -26,14 +26,55 @@ const menuItemsToDisplay = [
   { name: 'Panna Cotta', price: '$5.00', id: '21V' },
 ];
 
-const Item = ({ name, price }) => (
+const menuItemsToDisplay = [
+  {
+    title: 'Appetizers',
+    data: [
+      'Hummus',
+      'Moutabal',
+      'Falafel',
+      'Marinated Olives',
+      'Kofta',
+      'Eggplant Salad',
+    ],
+  },
+  {
+    title: 'Main Dishes',
+    data: ['Lentil Burger', 'Smoked Salmon', 'Kofta Burger', 'Turkish Kebab'],
+  },
+  {
+    title: 'Sides',
+    data: [
+      'Fries',
+      'Buttered Rice',
+      'Bread Sticks',
+      'Pita Pocket',
+      'Lentil Soup',
+      'Greek Salad',
+      'Rice Pilaf',
+    ],
+  },
+  {
+    title: 'Desserts',
+    data: ['Baklava', 'Tartufo', 'Tiramisu', 'Panna Cotta'],
+  },
+];
+
+
+const Item_old = ({ name, price }) => (
   <View style={menuStyles.innerContainer}>
     <Text style={menuStyles.itemText}>{name}</Text>
     <Text style={menuStyles.itemText}>{price}</Text>
   </View>
 );
 
-const MenuItems = () => {
+const Item = ({ name }) => (
+      <View style={menuStyles.innerContainer}>
+       <Text style={menuStyles.itemText}>{name}</Text>
+      </View>
+  );
+
+const MenuItems_old = () => {
   const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
 
   return (
@@ -46,7 +87,29 @@ const MenuItems = () => {
   );
 };
 
-const menuStyles = StyleSheet.create({
+const MenuItems = () => {
+  const renderItem = ({ item }) => <Item name={item} />;
+
+  const renderSectionHeader = ({ section: { title } }) => (
+    <Text style={menuStyles.sectionHeader}> { title } </Text>
+  );
+
+  return (
+    <View style={menuStyles.container}>
+      <SectionList
+        keyExtractor={(item, index) => item + index}
+        sections={menuItemsToDisplay}
+        renderItem={renderItem}
+        renderSectionHeader={renderSectionHeader}
+        ListFooterComponent={Footer}
+        ItemSeparatorComponent={Separator}
+        >
+       </SectionList>
+    </View>
+  );
+};
+
+const menuStyles_old = StyleSheet.create({
   container: {
     flex: 1,
   },
@@ -59,6 +122,46 @@ const menuStyles = StyleSheet.create({
   itemText: {
     color: '#F4CE14',
     fontSize: 20,
+  },
+});
+
+const Separator = () => <View style={menuStyles.separator} />;
+
+const Footer = () => (
+  <Text style={menuStyles.footerText}>
+    All Rights Reserved by Little Lemon 2022
+  </Text>
+);
+
+const menuStyles = StyleSheet.create({
+  container: {
+    flex: 0.95,
+  },
+  innerContainer: {
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    backgroundColor: '#333333',
+  },
+  sectionHeader: {
+    backgroundColor: '#fbdabb',
+    color: '#333333',
+    fontSize: 34,
+    flexWrap: 'wrap',
+    textAlign: 'center',
+  },
+  itemText: {
+    color: '#F4CE14',
+    fontSize: 32,
+  },
+  separator: {
+    borderBottomWidth: 1,
+    borderColor: '#EDEFEE',
+  },
+  footerText: {
+    color: '#EDEFEE',
+    fontSize: 20,
+    flexWrap: 'wrap',
+    textAlign: 'center',
   },
 });
 
