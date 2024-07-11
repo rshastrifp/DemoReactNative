63411934bfad0c1153299a6fc0ee90b2fd87adf5
Ronak Shastri <rshastrifp@gmail.com>
2024-07-11 19:32:38 -0400

    add phone number on Feedback form page

--- ios/components/FeedbackForm.js
+++ ios/components/FeedbackForm.js
@@ -1,50 +1,67 @@
 import React, { useState } from 'react'; 
 
-import { ScrollView, TextInput, StyleSheet, Text } from "react-native";
+import { 
+    ScrollView, 
+    TextInput, 
+    StyleSheet, 
+    Text, 
+    KeyboardAvoidingView, 
+    Platform} from "react-native";
 
 
 const FeedbackForm = () => {
 
     const [firstName, onChangeFirstName] = useState(''); 
     const [lastName, onChangeLastName] = useState(''); 
+    const [phoneNumber, onChangePhoneNumber] = useState(''); 
     const [message, onChangeMessage] = useState(''); 
-    const [phoneNumber, onChangePhoneNumber] = useState('');
 
     return(
-        <ScrollView style={styles.container}>
-        <Text style={styles.headingSection}>
-        How was your visit to Little Lemon?
-        </Text>
+        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'} >
+            <ScrollView style={styles.container}>
+            <Text style={styles.headingSection}>
+            How was your visit to Little Lemon?
+            </Text>
 
-        <Text style={styles.infoSection}> 
-        Little Lemon is a charming neighborhood bistro that serves simple food 
-        and classic cocktails in a lively but casual environment. We would love 
-        to hear your experience with us! 
-      </Text>
+            <Text style={styles.infoSection}> 
+            Little Lemon is a charming neighborhood bistro that serves simple food 
+            and classic cocktails in a lively but casual environment. We would love 
+            to hear your experience with us! 
+          </Text>
 
-        <TextInput
-            style={styles.input}
-            value={firstName}
-            onChangeText={onChangeFirstName}
-            placeholder='First Name'
-        />
+            <TextInput
+                style={styles.input}
+                value={firstName}
+                onChangeText={onChangeFirstName}
+                placeholder='First Name'
+            />
 
-        <TextInput
-            style={styles.input}
-            value={lastName}
-            onChangeText={onChangeLastName}
-            placeholder='Last Name'
-        />
+            <TextInput
+                style={styles.input}
+                value={lastName}
+                onChangeText={onChangeLastName}
+                placeholder='Last Name'
+            />
 
-        <TextInput
-            style={styles.input}
-            value={message}
-            onChangeText={onChangeMessage}
-            placeholder='Message'
-        />
+            <TextInput
+                style={styles.input}
+                value={phoneNumber}
+                onChangeText={onChangePhoneNumber}
+                keyboardType={"phone-pad"}
+                placeholder='Phone Number'
+            />
 
-        </ScrollView>
-    );
+            <TextInput
+                style={styles.messageInput}
+                value={message}
+                onChangeText={onChangeMessage}
+                placeholder='Message'
+                multiline={true}
+                maxLength={250}
+            />
+            </ScrollView>
+        </KeyboardAvoidingView>
+        );
 };
 
 export default FeedbackForm
@@ -68,7 +85,7 @@ const styles = StyleSheet.create({
             borderWidth: 1, 
             padding: 10, 
             fontSize: 16, 
-            backgroundColor: '#F4CE14', 
+            backgroundColor: '#EDEFEE', 
           }, 
           infoSection: { 
             fontSize: 24, 
